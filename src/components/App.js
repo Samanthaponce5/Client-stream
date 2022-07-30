4d0a0fda3822accd09adc3d9076dea2cc2c9b3d9
Samanthaponce5 <samanthaponce5@gmail.com>
2022-07-30 18:53:15 -0400

    redux-forms

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import  { BrowserRouter, Route, Link} from 'react-router-dom'
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+     <BrowserRouter>
+      <div className='ui container'>
+        <Header />
+        <Route exact path='/' component={StreamList}/>
+        <Route exact path='/streams/new' component={StreamCreate} />
+        <Route exact path='/streams/edit' component={StreamEdit} />
+        <Route exact path='/streams/delete' component={StreamDelete} />
+        <Route exact path='/stream/show' component={StreamShow} />
+      </div>
+     
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default   App;
